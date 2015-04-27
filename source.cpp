a016a66e281d6e6932a41d0b32bf6a90a97d533f
Andrey <zertsalov96@yandex.ru>
2015-04-27 11:43:40 +0300

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,13 @@
+#include "lcs.h"
+#include "testing.h"
+
+using namespace std;
+
+int main()
+{
+    unsigned int numberOfTests;
+    cin >> numberOfTests;
+    testing(numberOfTests);
+    system("pause");
+    return 0;
+}
